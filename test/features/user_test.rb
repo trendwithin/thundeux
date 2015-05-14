958ff976772ca1e2a3faf38cc0bc8282e26e6e51
Michael Becco <e4e6d4d5@gmail.com>
2015-05-14 15:27:56 -0700

    Pass User Creation

--- test/features/user_test.rb
+++ test/features/user_test.rb
@@ -4,9 +4,10 @@ feature "User" do
   scenario "Signs up" do
     visit root_path
     click_link 'Sign up'
-    #fill_in 'Email', with: users(:user_1).email
-    # fill_in 'Password', with: users(:user_1).password
-    # fill_in 'Password confirmation', with: users(:user_1).password
-    # click_button 'Sign up'
+    fill_in 'Email', with: 'maude@harrold.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    within(:css, 'div.actions') { click_button 'Sign up' }
+    page.must_have_content 'Welcome! You have signed up successfully.'
   end
 end
