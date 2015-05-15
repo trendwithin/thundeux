12aa795ee2c9c76ce5a8804c0d18430cf6b5999f
Michael Becco <e4e6d4d5@gmail.com>
2015-05-14 19:31:55 -0700

    Rename File

---
+++ test/features/user_signup_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+feature "User" do
+  scenario "Signs up" do
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: 'maude@harrold.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    within(:css, 'div.actions') { click_button 'Sign up' }
+    page.must_have_content 'Welcome! You have signed up successfully.'
+  end
+end
+
+feature "User Signs Up" do
+  scenario 'Invalid Password Confirmation' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'harold.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password1'
+    within(:css, 'div.actions') { click_button 'Sign up' }
+    page.must_have_content 'Password confirmation doesn\'t match Password'
+  end
+end
