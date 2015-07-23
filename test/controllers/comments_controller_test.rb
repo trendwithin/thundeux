e646a433e2a687c83515e8ae7727c381ad274996
Michael Becco <e4e6d4d5@gmail.com>
2015-07-22 23:17:54 -0700

    Add Comments Controller and Add Comment Test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,10 +1,27 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionController::TestCase
-  def test_sanity
-    skip
-    puts "Straight jacket fits"
+
+  before do
+    @memory = memories(:one)
   end
+
+  test 'POST: Adds a Comment To an Existing Memory' do
+    @user = users(:shane)
+    sign_in @user
+    assert_difference('Comment.count') do
+      post :create, comment: { body: 'Added a Comment'}, memory_id: @memory
+      assert_redirected_to memory_path(@memory.id, assigns(:story))
+    end
+  end
+
+  test 'POST: Unregistered User Can Not Make Comments' do
+    refute_difference('Comment.count') do
+      post :create, comment: { body: 'Added a Comment' }, memory_id: @memory
+      assert_redirected_to root_path
+    end
+  end
+
 
   # test 'new memory' do
   #   get :new
