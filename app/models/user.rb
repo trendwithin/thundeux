998c9a9711432de1a97d69ee91db6e4f59022e08
Michael Becco <e4e6d4d5@gmail.com>
2015-07-23 09:13:03 -0700

    Add UID and Provider to Users, Add Omniauthable to User and Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :email, length: { maximum: 255 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
