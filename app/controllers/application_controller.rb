06426636d5db6bd982a1c9d12b9906d9595b6ea6
Michael Becco <e4e6d4d5@gmail.com>
2015-07-23 14:06:27 -0700

    Modify Controllers and User for Twitter OAuth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   include Pundit
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  protected
+    def after_sign_in_path_for(_resource)
+      memories_path
+    end
   private
 
   def user_not_authorized
