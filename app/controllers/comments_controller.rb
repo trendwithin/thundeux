c0b129e57be057b2b2c0bbcdc7bf84aa9c7f6889
Michael Becco <e4e6d4d5@gmail.com>
2015-07-30 16:04:33 -0700

    Modify Create- Add Update and Destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,39 @@ class CommentsController < ApplicationController
   def create
     @memory = Memory.find(params[:memory_id])
     @comment = @memory.comments.build(comment_params)
+    @comment.approved = true if current_user == @memory.user
     authorize @comment
     if @comment.save
-      redirect_to memory_path(@memory), notice: 'Comment sent'
+      redirect_to memory_path(@memory), notice: 'Comment Sent, Awaiting Approval.'
     else
       redirect_to @memory, alert: 'Unable to send the comment', notice: 'Comment can\'t be blank'
+    end
+  end
+
+  def update
+    @memory = Memory.find(params[:memory_id])
+    @comment = Comment.find(params[:id])
+    authorize @comment
+    if @comment.update(comment_params)
+      redirect_to memory_path(@memory), notice: 'Comment Approved.'
+    else
+      redirect_to @memory, alert: 'Unable to approve comment', notice: 'Unable to approve comment'
+    end
+  end
+
+  def destroy
+    @memory = Memory.find(params[:memory_id])
+    @comment = Comment.find(params[:id])
+    authorize @comment
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @memory, notice: 'Comment Declined.' }
     end
   end
 
   private
 
     def comment_params
-      params.require(:comment).permit(:body, :memory_id)
+      params.require(:comment).permit(:body, :memory_id, :approved)
     end
 end
