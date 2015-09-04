99f84d17907d24c956a92b94affd78758de7f9d2
Michael Becco <e4e6d4d5@gmail.com>
2015-09-04 10:23:05 -0700

    Feature Memory View Test
    Test Related to User View Upon Login

---
+++ test/features/memory/view/homepage_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+feature "Memory Home Page View" do
+  before do
+    @user = users(:shane)
+  end
+  scenario "the page should have related memories" do
+    login @user
+    page.must_have_content 'Related Memories'
+    page.must_have_content 'Carnival Glass'
+  end
+end
