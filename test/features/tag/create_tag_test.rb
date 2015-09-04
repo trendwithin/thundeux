135c48f1848df81fe66c58b41f676f561241cd7f
Michael Becco <e4e6d4d5@gmail.com>
2015-09-03 18:47:47 -0700

    Create Tag Test

---
+++ test/features/tag/create_tag_test.rb
@@ -0,0 +1,41 @@
+require "test_helper"
+
+feature "Tag::CreateTag" do
+  before do
+    @user = User.new(email: 'us@them.com', password: 'password', role: 'registered')
+    @user.save
+  end
+
+  scenario "User adds Tags to their Memory" do
+    login @user
+    visit new_memory_path
+    fill_in "Name", with: "A New Memory"
+    fill_in "Description", with: "A New Description"
+    fill_in "Tags", with: "Vintage Radio"
+    click_on "Create Memory"
+    page.must_have_content "A New Memory"
+    page.must_have_content "A New Description"
+    page.must_have_content "Vintage Radio"
+  end
+
+  scenario "User adds comma separated tags" do
+    login @user
+    visit new_memory_path
+    fill_in "Name", with: "A New Memory"
+    fill_in "Description", with: "A New Description"
+    fill_in "Tags", with: "Vintage Radio, 1950s"
+    click_on "Create Memory"
+    page.must_have_content "A New Memory"
+    page.must_have_content "A New Description"
+    page.must_have_content "Vintage Radio, 1950s"
+  end
+
+  scenario "User Leaves Tags Blank" do
+    login @user
+    visit new_memory_path
+    fill_in "Name", with: "A New Memory"
+    fill_in "Description", with: "A New Description"
+    click_on "Create Memory"
+    page.must_have_content "Tag can't be blank."
+  end
+end
